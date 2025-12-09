97e24811c3887cf136adb59893c0d7b46c9efae4
bobabdo <abdo7.n1@gmail.com>
2025-12-09 10:23:54 +0500

    da

---
+++ src/Products.jsx
@@ -0,0 +1,88 @@
+
+
+export default function Products() {
+
+    return (
+        <>
+       <div className="bbb">
+         <div className="our">
+         <div className="our_text">
+<div>
+    <h1><span className="green">Our</span> Products</h1>
+    <ul>
+        <li><a href="#"><b>All</b></a></li>
+        <li><a href="#">Bed</a></li>
+        <li><a href="#">Chair</a></li>
+        <li><a href="#">Light</a></li>
+        <li><a href="#">Sofa</a></li>
+    </ul>
+</div>
+<a id="seal" href="">See All     </a>
+         </div>
+         <div className="mebel">
+    <div className="mebel_box">
+        <div className="mebel_text">
+     <div>
+        <p>Chair</p>
+        <h3>Glossy Cube</h3>
+     </div>
+     <img src="src/assets/Frame 39.png" alt="" />
+        </div>
+            <div className="mebel_img">
+                <img src="src/assets/unnamed (5) 1.png" alt="" />
+            </div>
+            <div>
+                <button>Uzs 300.000</button>
+            </div>
+    </div>
+    <div className="mebel_box">
+        <div className="mebel_text">
+     <div>
+        <p>Chair</p>
+        <h3>Glossy Cube</h3>
+     </div>
+     <img src="src/assets/Frame 39.png" alt="" />
+        </div>
+            <div className="mebel_img">
+                <img src="src/assets/unnamed (5) 1.png" alt="" />
+            </div>
+            <div>
+                <button>Uzs 300.000</button>
+            </div>
+    </div>
+    <div className="mebel_box">
+        <div className="mebel_text">
+     <div>
+        <p>Chair</p>
+        <h3>Glossy Cube</h3>
+     </div>
+     <img src="src/assets/Frame 39.png" alt="" />
+        </div>
+            <div className="mebel_img">
+                <img src="src/assets/unnamed (5) 1.png" alt="" />
+            </div>
+            <div>
+                <button>Uzs 300.000</button>
+            </div>
+    </div>
+    <div className="mebel_box">
+        <div className="mebel_text">
+     <div>
+        <p>Chair</p>
+        <h3>Glossy Cube</h3>
+     </div>
+     <img src="src/assets/Frame 39.png" alt="" />
+        </div>
+            <div className="mebel_img">
+                <img src="src/assets/unnamed (5) 1.png" alt="" />
+            </div>
+            <div>
+                <button>Uzs 300.000</button>
+            </div>
+    </div>
+         </div>
+        </div>
+       </div>
+        </>
+    )
+}
